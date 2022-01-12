03d54a0578232f8ab95e6de2963990b8b29c810e
Tomek Jadrzak <tim1977@gazeta.pl>
2022-01-12 20:20:07 +0100

    First tutorial

---
+++ RabbitMQ/Client/Program.cs
@@ -0,0 +1,45 @@
+﻿try
+{
+    ConnectionFactory connectionFactory =
+        new ConnectionFactory()
+        {
+            HostName = "localhost",
+            VirtualHost = "dev1",
+            UserName = "mqtest_dev1",
+            Password = "mqtest",
+        };
+
+    using var connection = connectionFactory.CreateConnection();
+    using var channel = connection.CreateModel();
+    
+    var properties = channel.CreateBasicProperties();
+    properties.Persistent = true;
+
+    while (true)
+    {
+        Console.Write("> ");
+        string? message = Console.ReadLine();
+        if (string.IsNullOrEmpty((message)))
+        {
+            break;
+        }
+
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(
+            "",
+            "testdev1", 
+            false, 
+            properties, 
+            body
+        );
+
+        Console.WriteLine("Send");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+Console.WriteLine("End");
---
+++ RabbitMQ/Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client.Events;
+
+try
+{
+    ConnectionFactory connectionFactory =
+        new ConnectionFactory()
+        {
+            HostName = "localhost",
+            VirtualHost = "dev1",
+            UserName = "mqtest_dev1",
+            Password = "mqtest",
+        };
+
+    using var connection = connectionFactory.CreateConnection();
+    using var channel = connection.CreateModel();
+
+    var consumer = new EventingBasicConsumer(channel);
+    
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(" [x] Received {0}", message);
+    };
+    
+    channel.BasicConsume(
+        "testdev1",
+        true,
+        "test-tag",
+        false,
+        false,
+        null,
+        consumer);
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+Console.WriteLine("End");
